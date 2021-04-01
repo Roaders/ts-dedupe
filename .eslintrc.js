1cc93dca153b9c6e6128b5126e2122ce026dbe85
GIles Roadnight <giles.roadnight@gmail.com>
2021-04-01 17:06:03 +0100

    Setup project / linting

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['dist/'],
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1 }],
+        semi: ['error', 'always'],
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+                semi: true,
+                trailingComma: 'all',
+                singleQuote: true,
+                printWidth: 120,
+                tabWidth: 4,
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off',
+            },
+        },
+    ],
+};
